a0e36c3e747e5e2d37c9ee4da48e5ca3856bf162
mozes-lol <63577510+mozes-lol@users.noreply.github.com>
2022-09-13 10:59:25 +0800

    refactor number input function

--- goofy calculator/goofyCalculator/src/main/java/com/mycompany/goofycalculator/goofyCalculatorGUI.java
+++ goofy calculator/goofyCalculator/src/main/java/com/mycompany/goofycalculator/goofyCalculatorGUI.java
@@ -274,39 +274,42 @@ public class goofyCalculatorGUI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void addDigit(int digit){
+        jTextField1_Main.setText(jTextField1_Main.getText() + String.valueOf(digit));
+    }
+    
     private void jButton_5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_5ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "5");
+        addDigit(5);
     }//GEN-LAST:event_jButton_5ActionPerformed
 
     private void jButton_6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_6ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "6");
+        addDigit(6);
     }//GEN-LAST:event_jButton_6ActionPerformed
 
     private void jButton_1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_1ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "1"); 
+        addDigit(1); 
     }//GEN-LAST:event_jButton_1ActionPerformed
 
     private void jButton_2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_2ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "2");
+        addDigit(2);
     }//GEN-LAST:event_jButton_2ActionPerformed
         
     private void jButton_3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_3ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "3");
+        addDigit(3);
     }//GEN-LAST:event_jButton_3ActionPerformed
 
     private void jButton_0ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_0ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "0");
+        addDigit(0);
     }//GEN-LAST:event_jButton_0ActionPerformed
 
     private void jButton_EqualsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_EqualsActionPerformed
         // TODO add your handling code here:
-        
     }//GEN-LAST:event_jButton_EqualsActionPerformed
 
     private void jButton_AddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_AddActionPerformed
@@ -336,22 +339,22 @@ public class goofyCalculatorGUI extends javax.swing.JFrame {
 
     private void jButton_7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_7ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "7");
+        addDigit(7);
     }//GEN-LAST:event_jButton_7ActionPerformed
 
     private void jButton_8ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_8ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "8");
+        addDigit(8);
     }//GEN-LAST:event_jButton_8ActionPerformed
 
     private void jButton_9ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_9ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "9");
+        addDigit(9);
     }//GEN-LAST:event_jButton_9ActionPerformed
 
     private void jButton_4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_4ActionPerformed
         // TODO add your handling code here:
-        jTextField1_Main.setText(jTextField1_Main.getText() + "4");
+        addDigit(4);
     }//GEN-LAST:event_jButton_4ActionPerformed
 
     /**
